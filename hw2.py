20550c57294d3fa3ef8fc8b7230e6f0f6491bd37
Iryna-hear <kediryna@gmail.com>
2025-02-13 16:05:50 +0100

    Add files via upload

---
+++ hw2.py
@@ -0,0 +1,57 @@
+# #1a
+# a = 'some string'
+# print(len(a))
+# #1b
+# a = 'I am '
+# b = 'live'
+# print(a + b)
+# #2c
+# num = 4
+# print(num ** 2)
+# #2d
+# numb_1 = 3
+# numb_2 = 5
+# print(numb_1 + numb_2)
+# #2e
+# num_1 = 4
+# num_2 = 6
+# print(num_1 // num_2)
+# print(num_1 % num_2)
+# #3f
+# my_list = [4, 5, 3, 8, 10]
+# print(sum(my_list)/ len(my_list))
+# #3g
+# list_1  = [2, 'three', 'apple', 7, 5]
+# list_2 = ['simple', 'three', 4,  'apple', 'dog', 5]
+# print(set(list_1) & set(list_2))
+# #4h
+# dict_1 = {"name" : "Stiv", "age" : "34", "job" : "seller", "musik" : "rock"}
+# for key in dict_1:
+#     print(key)
+# #4i
+# dict_1 = {"name" : "Stiv", "age" : "34"}
+# dict_2 = {"job" : "seller", "musik" : "rock"}
+# result= dict_1 | dict_2
+# print(result)
+# #5j
+# set_q = {'milk', 'solt', 'meet'}
+# q_set = {'hell', 'bus', 'stone'}
+# print(set_q | q_set)
+# #5 
+# set_q = {'milk', 'solt', 'meet', 'coffee'}
+# q_set = {'hell', 'bus', 'stone'}
+# result = set_q.issubset(q_set)
+# print(result)
+# #6
+# number = 7
+# if number % 2 ==0:
+#     print("Even")
+# else:
+#     print("Not even")
+# #6
+# some_list = [2, 3, 4, 5, 6, 7, 8]
+# for i in some_list:
+#     if i % 2 == 0:
+#         print(i)
+
+   
